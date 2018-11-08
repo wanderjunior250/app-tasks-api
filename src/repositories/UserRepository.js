73c0dcdc4a1fd40eb629c9c2a6e9327e66008aa5
Ruizera <victorruizleopoldino@gmail.com>
2018-11-08 21:56:02 -0200

    GO HORSE

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 var User = require('../config/db');
 
 
-module.exports = new class userRepository {
+module.exports = new class UserRepository {
 
   getAll() {
     return User.find();
