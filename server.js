7e2f308026c33e5e4983d3f2da59928676598765
Wander Junior <wanderjunior@live.com>
2018-11-08 18:45:00 -0200

    change server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || 5000;
 
 app.listen(port, function () {
   console.log(`app listening on port ${port}`)
