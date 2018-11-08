1f7b19efd82b2e5170b60693ee83a95d4b80a8b7
Wander Junior <wanderjunior@live.com>
2018-11-08 13:27:22 -0200

    change port

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 const app = require('../src/app');
 
-const port = normalizaPort(process.env.PORT || '3000');
+const port = normalizaPort(process.env.PORT || '8080');
 
 function normalizaPort(val) {
   const port = parseInt(val, 10);
